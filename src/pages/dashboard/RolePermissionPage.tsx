8bbdcb33f1fd246db9e93a7db544baba802cec47
Gauthier7312 <koffigauthierkader@gmail.com>
2023-10-21 16:08:13 +0200

    design others menu

--- src/pages/dashboard/RolePermissionPage.tsx
+++ src/pages/dashboard/RolePermissionPage.tsx
@@ -1,10 +1,18 @@
-import React from 'react';
 import PageContainer from '../../components/PageContainer';
+import { Button } from '@mui/material';
+import { TableComponent } from '../../components/table';
 
 export default function RolePermissionPage() {
   return (
-    <PageContainer menu="Role et permissions">
-      <div>Role</div>
+    <PageContainer
+      menu="Role et permissions"
+      headerButton={
+        <Button size="large" variant="contained">
+          Ajouter
+        </Button>
+      }
+    >
+      <TableComponent />
     </PageContainer>
   );
 }
